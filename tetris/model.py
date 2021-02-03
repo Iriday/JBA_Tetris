58f1857cb6e876d584fe35f078e4e383a0f708ef
Igor <GI_Something@protonmail.com>
2021-02-03 11:50:18 +0200

    feat: Make some changes to simplify impl of collision detection

--- tetris/model.py
+++ tetris/model.py
@@ -14,17 +14,31 @@ class Model:
     def __init__(self, dimensions):
         self.field_width, self.field_height = dimensions
         self.game_filed = np.zeros((self.field_height, self.field_width), dtype=np.int8)
-        self.empty_piece = np.zeros((4, 4))
 
     def start_round(self, piece_name):  # temp
         self.curr_piece_name = piece_name
         self.curr_piece_index = 0
-        self.curr_piece_coords = self.calc_piece_start_pos()
-        self.place_piece(self.curr_piece_coords, PIECES[piece_name][self.curr_piece_index])  # place piece at start pos
+        self.piece_offset = 0
+        self.curr_piece_coords = self.adjust_piece_coords(PIECES[self.curr_piece_name][self.curr_piece_index].copy(), self.piece_offset, self.field_width)
+        self.place_piece(self.curr_piece_coords, 1)  # place piece at start pos
 
-    def calc_piece_start_pos(self):
-        half = self.field_width // 2 - 2
-        return np.arange(self.field_width * 4).reshape((4, self.field_width))[:, half:(half + 4)]  # return indexes
+    def adjust_piece_coords(self, piece_4x4_indexes, piece_offset, field_width):
+        len_before = field_width // 2 - 2  # left upper corner (len before piece)
+        len_after = field_width - len_before - 4
+
+        with np.nditer(piece_4x4_indexes, op_flags=["readwrite"]) as indexes:
+            for i in indexes:
+                if i <= 3:
+                    i += len_before
+                elif i <= 7:
+                    i += len_before * 2 + len_after
+                elif i <= 11:
+                    i += len_before * 3 + len_after * 2
+                elif i <= 15:
+                    i += len_before * 4 + len_after * 3
+                i += piece_offset
+
+        return piece_4x4_indexes
 
     def place_piece(self, indexes, piece):
         self.game_filed.put(indexes, piece, mode='clip')
@@ -32,23 +46,25 @@ class Model:
     def move_piece(self, action):
         def rotate():
             self.curr_piece_index = (self.curr_piece_index + 1) % 4
+            self.curr_piece_coords = self.adjust_piece_coords(PIECES[self.curr_piece_name][self.curr_piece_index].copy(), self.piece_offset, self.field_width)
 
         def down():
+            self.piece_offset += self.field_width
             self.curr_piece_coords += self.field_width
 
         def left():
-            cols = self.curr_piece_coords[0:1, 0:4].flatten() % self.field_width == 0
-            if np.any(cols):
-                self.curr_piece_coords[:, cols] += self.field_width
+            self.piece_offset -= 1
+            self.curr_piece_coords[self.curr_piece_coords % self.field_width == 0] += self.field_width
             self.curr_piece_coords -= 1
 
         def right():
+            self.piece_offset += 1
             self.curr_piece_coords += 1
-            cols = self.curr_piece_coords[0:1, 0:4].flatten() % self.field_width == 0
+            cols = self.curr_piece_coords % self.field_width == 0
             if np.any(cols):
-                self.curr_piece_coords[:, cols] -= self.field_width
+                self.curr_piece_coords[cols] -= self.field_width
 
-        self.place_piece(self.curr_piece_coords, self.empty_piece)  # remove previous piece
+        self.place_piece(self.curr_piece_coords, 0)  # remove previous piece
 
         if action == Action.ROTATE:
             down()
@@ -62,4 +78,4 @@ class Model:
             down()
             right()
 
-        self.place_piece(self.curr_piece_coords, PIECES[self.curr_piece_name][self.curr_piece_index])
+        self.place_piece(self.curr_piece_coords, 1)
