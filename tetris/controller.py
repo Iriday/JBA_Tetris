79afa488cf404c5e82ee417420badeec6d6035c3
Igor <GI_Something@protonmail.com>
2021-02-04 10:29:13 +0200

    feat: Impl collision detection

--- tetris/controller.py
+++ tetris/controller.py
@@ -3,9 +3,9 @@ import model
 
 piece_name = v.get_piece_name()
 m = model.Model(v.get_dimensions())
-v.show_game_field(m.game_field)  # show empty field
+v.show_game_field(m.get_game_field())  # show empty field
 m.start_round(piece_name)
 
 while True:
-    v.show_game_field(m.game_field)
+    v.show_game_field(m.get_game_field())
     m.move_piece(v.get_action())
--- tetris/model.py
+++ tetris/model.py
@@ -16,46 +16,78 @@ class Action(Enum):
 
 class Model:
     def __init__(self, dimensions):
-        self.field_width, self.field_height = dimensions
-        self.game_field = np.zeros((self.field_height, self.field_width), dtype=np.int8)
+        self.field_width = dimensions[0] + 2  # +2 walls
+        self.field_height = dimensions[1] + 1  # + floor
+        self.game_field = Model._create_game_field(self.field_width, self.field_height)
 
     def start_round(self, piece_name):  # temp
         self.piece_name = piece_name
         self.piece_state = 0
         self.piece_offset = 0
-        self.piece_indexes = Model._adjust_piece_indexes(PIECES[self.piece_name][self.piece_state].copy(), self.piece_offset, self.field_width)
+        self.__adjust_indexes = lambda: Model._adjust_piece_indexes(PIECES[self.piece_name][self.piece_state].copy(),
+                                                                    self.piece_offset, self.field_width)
+        self.piece_indexes = self.__adjust_indexes()
+        self.piece_frozen = False
         Model._draw_piece(self.game_field, self.piece_indexes, 1)  # place piece at start pos
 
     def move_piece(self, action):
+        if self.piece_frozen:
+            return
+
         def rotate():
             self.piece_state = (self.piece_state + 1) % 4
-            self.piece_indexes = self._adjust_piece_indexes(PIECES[self.piece_name][self.piece_state].copy(), self.piece_offset, self.field_width)
+            self.piece_indexes = self.__adjust_indexes()
+            if Model._collision_detected(self.game_field, self.piece_indexes):  # revert piece move
+                self.piece_state = (self.piece_state + 3) % 4
+                self.piece_indexes = self.__adjust_indexes()
 
         def down():
-            self.piece_offset += self.field_width
             self.piece_indexes += self.field_width
+            if Model._collision_detected(self.game_field, self.piece_indexes):  # revert piece move
+                self.piece_indexes -= self.field_width
+                return
+            self.piece_offset += self.field_width
 
         def left():
-            self.piece_offset -= 1
-            self.piece_indexes[self.piece_indexes % self.field_width == 0] += self.field_width
             self.piece_indexes -= 1
+            if Model._collision_detected(self.game_field, self.piece_indexes):  # revert piece move
+                self.piece_indexes += 1
+                return
+            self.piece_offset -= 1
 
         def right():
-            self.piece_offset += 1
             self.piece_indexes += 1
-            self.piece_indexes[self.piece_indexes % self.field_width == 0] -= self.field_width
+            if Model._collision_detected(self.game_field, self.piece_indexes):  # revert piece move
+                self.piece_indexes -= 1
+                return
+            self.piece_offset += 1
 
         Model._draw_piece(self.game_field, self.piece_indexes, 0)  # remove previous piece
 
-        down()  # also if action == Action.DOWN:
         if action == Action.ROTATE:
             rotate()
         elif action == Action.LEFT:
             left()
         elif action == Action.RIGHT:
             right()
+        down()  # also if action == Action.DOWN:
+
+        # froze piece if it hit the floor
+        if Model._collision_detected(self.game_field, self.piece_indexes + self.field_width):
+            self.piece_frozen = True
 
         Model._draw_piece(self.game_field, self.piece_indexes, 1)
+
+    def get_game_field(self):
+        return self.game_field[0:self.field_height - 1, 1:self.field_width - 1]
+
+    # Static methods ------------------
+    @staticmethod
+    def _create_game_field(width, height):
+        field = np.zeros((height, width), dtype=np.int8)
+        field[:, [0, width - 1]] += 1
+        field[height - 1] += 1
+        return field
 
     @staticmethod
     def _adjust_piece_indexes(piece_4x4_indexes, piece_offset, field_width):
@@ -75,6 +107,10 @@ class Model:
                 i += piece_offset
 
         return piece_4x4_indexes
+
+    @staticmethod
+    def _collision_detected(game_field, piece):
+        return len(np.intersect1d(np.flatnonzero(game_field), piece)) > 0
 
     @staticmethod
     def _draw_piece(game_field, indexes, piece):
